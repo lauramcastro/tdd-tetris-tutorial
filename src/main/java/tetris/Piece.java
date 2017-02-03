8826752a36cebed130c0fe95ea9bd1b413c0bc9d
Laura M. Castro <lcastro@udc.es>
2017-02-03 12:58:29 +0100

    Implemented code to pass RotatingPiecesOfBlocksTest.can_be_rotated_right()

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -2,6 +2,9 @@ package tetris;
 
 public class Piece {
 
+    private static final String TWO_BLOCK_PIECE_VERTICAL   = ".X.\n.X.\n";
+    private static final String TWO_BLOCK_PIECE_HORIZONTAL = "XX\n";
+    
     private String piece;
     
     public Piece(String piece) {
@@ -10,6 +13,10 @@ public class Piece {
 
     public String toString() {
         return piece;
+    }
+
+    public Piece rotateRight() {
+        return new Piece("...\n" + Piece.TWO_BLOCK_PIECE_HORIZONTAL + "...\n");
     }
 
 }
