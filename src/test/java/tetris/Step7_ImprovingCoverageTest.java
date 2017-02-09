5c1e22b9cd4184dee6af17cef11f77835723deea
Laura M. Castro <lcastro@udc.es>
2017-02-09 11:31:28 +0100

    Added (momentarily dummy) class for tests to improve coverage.

---
+++ src/test/java/tetris/Step7_ImprovingCoverageTest.java
@@ -0,0 +1,15 @@
+package tetris;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Step7_ImprovingCoverageTest {
+
+    // TODO: add tests to get coverage to 100%
+    @Test
+    public void test_to_improve_coverage() {
+        assertTrue(true);
+    }
+
+}
